c6a1d2e9c45d32ff4e88bd0d416833380e7c858d
VT <github@intelleq.co.za>
2024-01-10 11:55:38 +0200

    setup lspsaga

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,4 +16,19 @@ return {
     event = 'VeryLazy',
   },
   { 'nvim-tree/nvim-web-devicons' },
+  {
+    'nvimdev/lspsaga.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('lspsaga').setup({})
+      vim.keymap.set('n', '<leader>d', '<cmd>Lspsaga diagnostic_jump_next<cr>')
+      vim.keymap.set('n', 'ga', '<cmd>Lspsaga code_action<cr>')
+      vim.keymap.set('n', 'gf', '<cmd>Lspsaga finder<cr>')
+      vim.keymap.set('n', 'gp', '<cmd>Lspsaga peek_definition<cr>')
+      vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<cr>')
+    end,
+  },
 }
