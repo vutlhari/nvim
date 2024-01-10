4ff3687ac742f76026aa6f3dc28ae10f357d532a
VT <github@intelleq.co.za>
2024-01-10 21:15:14 +0200

    collapse plugins into single file

--- init.lua
+++ init.lua
@@ -1,25 +1,25 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-vim.opt.guifont = { 'Dank Mono', ':h15' }
+vim.opt.guifont = { "Dank Mono", ":h15" }
 
 local opt = vim.opt
 
-vim.cmd [[
+vim.cmd([[
     filetype plugin indent on
     syntax on
-]]
+]])
 -- line number
 opt.number = true
 
@@ -38,32 +38,33 @@ opt.cursorline = true
 
 -- true color terminal settings
 opt.termguicolors = true
-opt.background = 'dark'
-opt.signcolumn = 'yes' -- enable specific highlights in debug mode
+opt.background = "dark"
+opt.signcolumn = "yes" -- enable specific highlights in debug mode
 
 -- backspace settings
-opt.backspace = 'indent,eol,start'
+opt.backspace = "indent,eol,start"
 -- enable native clipboard instead of vim default clipboard behavior
 -- opt.clipboard:append "unnamedplus"
-vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+vim.opt.clipboard:prepend({ "unnamed", "unnamedplus" })
 
 -- split windows
 opt.splitright = true
 opt.splitbelow = true
 
 -- set dash as normal letter instead of divide behavior
-opt.iskeyword:append '-'
+opt.iskeyword:append("-")
 
 opt.showmode = false
 
 -- auto completion menu height
 vim.opt.pumheight = 10
 
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set('n', '<leader><Tab>', ':bn<CR>', { desc = 'Next buffer' })
-vim.keymap.set('n', '<leader><S-Tab>', ':bp<CR>', { desc = 'Prev buffer' })
-vim.keymap.set('n', '<leader>bd', ':bd<CR>', { desc = 'Delete buffer' })
-vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename variable' })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<leader><Tab>", ":bn<CR>", { desc = "Next buffer" })
+vim.keymap.set("n", "<leader><S-Tab>", ":bp<CR>", { desc = "Prev buffer" })
+vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Delete buffer" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename variable" })
 
-require('lazy').setup('plugins');
-vim.cmd.colorscheme 'tokyonight-night'
+require("lazy").setup("plugins")
+
+vim.cmd.colorscheme("catppuccin-mocha")
