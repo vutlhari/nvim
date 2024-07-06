8ea7820a8a708df1e1f864d02cf3a6000f667fbb
vt <me@vtcodes.dev>
2024-07-06 20:02:35 +0200

    move keymaps to dedicated file

--- init.lua
+++ init.lua
@@ -58,12 +58,7 @@ opt.showmode = false
 -- auto completion menu height
 vim.opt.pumheight = 10
 
-vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-vim.keymap.set("n", "<leader><Tab>", ":bn<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<leader><S-Tab>", ":bp<CR>", { desc = "Prev buffer" })
-vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Delete buffer" })
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename variable" })
-
 require("lazy").setup("plugins")
+require("keymaps")
 
 vim.cmd.colorscheme("catppuccin-mocha")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -556,13 +556,6 @@ return {
     config = function()
       local harpoon = require("harpoon")
       harpoon:setup()
-
-      vim.keymap.set("n", "<leader>a", function()
-        harpoon:list():append()
-      end)
-      vim.keymap.set("n", "<leader>h", function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
     end,
   },
   {
