bcd64b8bc8694f3d70ab0d479697a2cfce75fef4
vt <me@vtcodes.dev>
2024-09-26 21:32:26 +0200

    visualize and work with indent scope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -62,6 +62,25 @@ return {
     end,
   },
   {
+    "echasnovski/mini.indentscope",
+    opts = {
+      symbol = "╎",
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = function()
+      return {
+        indent = { char = "•" },
+        whitespace = {
+          remove_blankline_trail = true,
+        },
+        scope = { enabled = false },
+      }
+    end,
+  },
+  {
     "lewis6991/gitsigns.nvim",
     opts = {
       signs = {
